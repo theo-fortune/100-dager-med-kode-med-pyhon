69362fc3b464e6fda37b71d584dfd949c3c3a41b
Theo Fortune <theofortunefunction@gmail.com>
2024-09-11 23:39:22 +0100

    day one added

--- day1.py
+++ day1.py
@@ -1,37 +1,42 @@
-# print("Hello" + " Fortune")
-# x = 2
-# y = 3
-# z = x + y
-# print(z)
-# print("Hello " + input("What is your name?"))
+print("Hello" + " Fortune")
+x = 2
+y = 3
+z = x + y
+print(z)
+print("Hello " + input("What is your name?"))
 
 # * Variables
-# name = input("What is your name?")
-# print(name)
+name = input("What is your name?")
+print(name)
 
 # * Challenge
 # * There are two variables, a and b from input
 
-# a = input();
-# b = input();
+a = input();
+b = input();
 
-# c = a
-# a = b
-# b = c
+c = a
+a = b
+b = c
 
-# print("a: " + a)
-# print("b: " + b)
+print("a: " + a)
+print("b: " + b)
 
 # # * Variable Naming
 
-# n = input("What is your name")
-# l = len(n)
-# print(l)
+n = input("What is your name")
+l = len(n)
+print(l)
 
-# user_name = "Fortune"
-# print(user_name)
+user_name = "Fortune"
+print(user_name)
 
 name = input("What is your name")
 city = input ("Which city were you born")
 
 print("The name of your Musical Band is " + name + city)
+
+x = int(input("Your first value "))
+y = int(input("Your second value "))
+z = x * y
+print("The product of " + str(x) + " and " + str(y) + " is " + str(z))
