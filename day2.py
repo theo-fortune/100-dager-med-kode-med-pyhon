4a0c0444b79eb90c06f8714849d0381517d8ddf1
Theo Fortune <theofortunefunction@gmail.com>
2024-09-13 11:54:37 +0100

    day2

--- day2.py
+++ day2.py
@@ -24,10 +24,32 @@ False
 
 # two_digit_number = first_digit + second_digit
 # print(two_digit_number)
-print(3 + 3 / 3 * 3 - 3)
+# print(3 + 3 / 3 * 3 - 3)
 
-height = input();
-weight = input();
+# height = input();
+# weight = input();
 
-bmi = int(weight) / (float(height) ** 2) 
-print(int(bmi))
+# bmi = int(weight) / (float(height) ** 2) 
+# print(int(bmi))
+
+result = 8 / 3
+# print(type(result))
+
+score = 3
+
+#* User scores a point
+score += 1
+print(score)
+
+height = 1.85
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+age = int(input())
+weeks_in_a_year = 52
+year90 = 90 * weeks_in_a_year
+weeks_lived = age * weeks_in_a_year
+weeks_remaining = year90 - weeks_lived
+message = f"you are {age} years old and you've lived {weeks_lived} weeks, you have {weeks_remaining} left"
+print(message)
